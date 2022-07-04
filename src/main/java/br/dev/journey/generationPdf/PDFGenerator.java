65b943e96b692419dee2ea8ca8e085beb4d32a7e
Nivaldo Hydalgo <nivaldohydalgo@gmail.com>
2022-07-03 21:29:41 -0300

    Second Commit

--- src/main/java/br/dev/journey/generationPdf/PDFGenerator.java
+++ src/main/java/br/dev/journey/generationPdf/PDFGenerator.java
@@ -64,12 +64,12 @@ public class PDFGenerator {
             String text2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
             document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
-            document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text2, "courier", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text, "times", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text2, "helvetica", 12f, "justified", true, false, 3f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, true, 3f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 8f, 0f, 0f, 0f));
+            document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 20f));
             document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
             document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
             document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
