8f8b1c046a8f1be0cb4294479c407abac3b55828
Nivaldo Hydalgo <nivaldohydalgo@gmail.com>
2022-07-06 20:02:26 -0300

    Commit com ZXing

--- src/main/java/br/dev/journey/generationPdf/PDFGenerator.java
+++ src/main/java/br/dev/journey/generationPdf/PDFGenerator.java
@@ -5,6 +5,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
@@ -19,6 +23,7 @@ import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.Style;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
@@ -47,7 +52,7 @@ public class PDFGenerator {
         Float qtcodeTopMargin = 130f;
 
         // Atributos padrões
-        Float maxWidthHeader = 380f;
+        Float maxWidthHeader = 370f;
 
         ByteArrayOutputStream saida = new ByteArrayOutputStream();
         PdfWriter writer = new PdfWriter(saida);
@@ -55,10 +60,12 @@ public class PDFGenerator {
         PdfDocument doc = new PdfDocument(writer);
 
         Document document = new Document(doc, PageSize.A4, false);
-        document.setTextAlignment(TextAlignment.JUSTIFIED);
+        document.setTextAlignment(TextAlignment.JUSTIFIED_ALL);
         document.setMargins(textMarginTop, textMarginRight, textMarginBottom, textMarginLeft);
 
         try {
+
+            processVarargIntegers("Nivaldo", "fontSize: 12");
 
             String text = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
             String text2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -91,6 +98,31 @@ public class PDFGenerator {
             document.add(criaParagrafo(text2, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
             document.add(criaParagrafo(text, "helvetica", 12f, "justified", false, false, 3f, 0f, 0f, 0f));
 
+            int width = 140;
+            int height = 140;
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            
+            BitMatrix bitMatrix = new BitMatrix(140, 140);
+            System.out.println("bitMatrix width....: " + bitMatrix.getWidth());
+            System.out.println("bitMatrix height...: " + bitMatrix.getHeight());
+
+            bitMatrix = qrCodeWriter.encode("25584523365542144587745552121455522200002556632221444000221441755214221254477", BarcodeFormat.QR_CODE, width, height);
+            // bitMatrix = qrCodeWriter.encode("02547552114171221212008765434156554432310000098765644343455564443321345667777", BarcodeFormat.QR_CODE, width, height);
+            //bitMatrix = qrCodeWriter.encode("QRCode do zé lezin é assim", BarcodeFormat.QR_CODE, width, height);
+            //bitMatrix = qrCodeWriter.encode("My QRCode", BarcodeFormat.QR_CODE, width, height);
+            //bitMatrix.setRegion(20, 20, width, height);
+            System.out.println("bitMatrix width....: " + bitMatrix.getWidth());
+            System.out.println("bitMatrix height...: " + bitMatrix.getHeight());
+            System.out.println("bitMatrix RowSize..: " + bitMatrix.getRowSize());
+            
+            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+            byte[] pngData = pngOutputStream.toByteArray(); 
+            ImageData qrCode = ImageDataFactory.create(pngData);
+
+
+
+
             // ******* INCLUIR HEADER E FOOTER NO PDF *******
             int qtPages = doc.getNumberOfPages();
             for (int i = 1; i <= qtPages; i++) {
@@ -110,13 +142,22 @@ public class PDFGenerator {
                         pageSize.getTop() - documentMarginTop, TextAlignment.LEFT);
 
                 // ****** INCLUI O QRCODE ******
-                File directory = new File("./");
-                String path = directory.getAbsolutePath() + "/src/assets/";
-                ImageData imageData = ImageDataFactory.create(path + "qrcode.jpg");
-                imageData.setWidth(qrcodeWidth);
-                imageData.setHeight(qrcodeHeight);
-                pdfCanvas.addImageAt(imageData, pageSize.getWidth() - (qrcodeWidth + documentMarginRight),
-                        pageSize.getTop() - qtcodeTopMargin, false);
+                //File directory = new File("./");
+                //String path = directory.getAbsolutePath() + "/src/assets/";
+
+                //qrCode.setWidth(qrcodeWidth);
+                //qrCode.setHeight(qrcodeHeight);
+                qrCode.setWidth(width);
+                qrCode.setHeight(height);
+                pdfCanvas.addImageAt(qrCode, pageSize.getWidth() - 158,
+                        pageSize.getTop() - 158, false);
+               
+
+//                ImageData imageData = ImageDataFactory.create(path + "qrcode.jpg");
+//                imageData.setWidth(qrcodeWidth);
+//                imageData.setHeight(qrcodeHeight);
+//                pdfCanvas.addImageAt(imageData, pageSize.getWidth() - (qrcodeWidth + documentMarginRight),
+//                        pageSize.getTop() - qtcodeTopMargin, false);
 
                 // ****** LINHA ABAIXO DO CABECALHO ******
                 pdfCanvas.setLineWidth(1)
@@ -227,6 +268,14 @@ public class PDFGenerator {
         }
 
         return p;
+    }
+
+    private static void processVarargIntegers(String... args) {
+ 
+        for (String arg : args) {
+            System.out.println("arg: " + arg);
+        }
+
     }
 
 }
